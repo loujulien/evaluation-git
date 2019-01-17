c5b9fdb875ac4c0704da0c77063ab588ba4eda57
Kashinax <ruben.lecomte@gmail.com>
2019-01-17 14:02:46 +0100

    Création Games : Controller + twig / Modification : PageController.php

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -5,16 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PageController extends AbstractController
+class GamesController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function home()
-    {
-        return $this->render('page/home.html.twig');
-    }
-
     /**
      * @Route("/games", name="games")
      */
